527dd3d145467758210100fd0e40db37139ec2a6
Matthew <matthew@creativengine.com>
2019-05-22 11:14:04 -0700

    identify ie 10 ie 11

--- detect-browser.js
+++ detect-browser.js
@@ -11,3 +11,29 @@ function detectEdge() {
 if (detectEdge()) {
   $('body').addClass('EDGE')
 }
+
+function getIEVersion() {
+  var agent = navigator.userAgent
+  var reg = /MSIE\s?(\d+)(?:\.(\d+))?/i
+  var matches = agent.match(reg)
+  if (matches != null) {
+    return {
+      major: matches[1],
+      minor: matches[2]
+    }
+  }
+  return {
+    major: '-1',
+    minor: '-1'
+  }
+}
+var ie_version = getIEVersion()
+var is_ie10 = ie_version.major == 10
+var is_ie11 = ie_version.major == 11
+if (is_ie10) {
+  $('body').addClass('IE10')
+}
+
+if (is_ie11) {
+  $('body').addClass('IE11')
+}
