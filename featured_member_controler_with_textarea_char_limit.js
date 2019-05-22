30044c31ca44a4e2771612386a8bd0767900a87c
Matthew <matthew@creativengine.com>
2019-05-22 09:27:35 -0700

    alert green when below maximum

--- featured_member_controler_with_textarea_char_limit.js
+++ featured_member_controler_with_textarea_char_limit.js
@@ -1,27 +1,26 @@
 var main_feature = null;
-window.addEvent("domready", function() {
-	main_feature = new List_Controller({
-		id: "main_feature",
-		target: "main-feature-container",
-		datasource: application_webroot + "cfc/core/lists/featured/original.cfc",
-		item_controller_class: Featured_Extended_List_Item_Controller,
-		site_path: "homepage",
-		page_section:"main_feature",
-		method: "get",
-		restrict_by_language: true,
-		form: {
-			id: "featured-form",
-			src: application_webroot + "script/list/controllers/featured/original/extended/form.cfm?headlinelabel=Featured_Member"
+window.addEvent("domready", function () {
+  main_feature = new List_Controller({
+    id: "main_feature",
+    target: "main-feature-container",
+    datasource: application_webroot + "cfc/core/lists/featured/original.cfc",
+    item_controller_class: Featured_Extended_List_Item_Controller,
+    site_path: "homepage",
+    page_section: "main_feature",
+    method: "get",
+    restrict_by_language: true,
+    form: {
+      id: "featured-form",
+      src: application_webroot + "script/list/controllers/featured/original/extended/form.cfm?headlinelabel=Featured_Member"
     },
 
-		item_controller_custom_options: {
-         do_descriptions: true,
-         do_image: true,
-         do_thumbnails: false
+    item_controller_custom_options: {
+      do_descriptions: true,
+      do_image: true,
+      do_thumbnails: false
     },
 
-  types: [
-    {
+    types: [{
       label: "Committee Members",
       views: {
         get: { // related view
@@ -35,49 +34,55 @@ window.addEvent("domready", function() {
           title_field: "full_name"
         }
       },
-      cols: [
-        { column: "full_name", print: true, label: "Name (party)", print_label_in_result: false, searchable: true, sortable: true, sort_column: true, sort_order: "ASC" }
-      ],
+      cols: [{
+        column: "full_name",
+        print: true,
+        label: "Name (party)",
+        print_label_in_result: false,
+        searchable: true,
+        sortable: true,
+        sort_column: true,
+        sort_order: "ASC"
+      }],
       selector_title: "Members",
       selector_description: "<p>Please select one member from the list below.</p>",
       selector_no_results_text: "<p>[ We're sorry, there were no members. ]</p>",
       amend_content_location_type: "member"
-    }
-	],
-	title: "Home Page Featured Member",
-	description: "<p>This is the featured member to display on the homepage</p>",
-	max_items: 2
+    }],
+    title: "Home Page Featured Member",
+    description: "<p>This is the featured member to display on the homepage</p>",
+    max_items: 2
   });
 });
 
 
-$(function(){
+$(function () {
   var el = $('#main-feature-container');
   var list = $('.control-list.sortable');
   var listItem = $('#main_feature-li-0');
 
-  if(!el || !list || !listItem) return;
+  if (!el || !list || !listItem) return;
 
-  setTimeout(function() {
+  setTimeout(function () {
     var button = $('#main_feature-li-0 .padder .links a.btn.listbuttons');
 
-    button.on('click', function(){
-      alert('past edit button');
+    button.on('click', function () {
+      // alert('past edit button');
       var summary = $('#main_feature-summary-0');
       console.log(summary);
 
-      if(!summary)return;
+      if (!summary) return;
 
-      summary.on('keyup',function() {
+      summary.on('keyup', function () {
         $('#the-count').remove();
         var count = '<div id="the-count">Maximum characters allowed: <span id="current">0</span><span id="maximum">/ 147</span></div>'
 
         $('label[for="main_feature-summary-0"]').append(count);
 
         var characterCount = $(this).val().length,
-            current = $('#current'),
-            maximum = $('#maximum'),
-            theCount = $('#the-count');
+          current = $('#current'),
+          maximum = $('#maximum'),
+          theCount = $('#the-count');
         current.text(characterCount);
 
         var maxElementCharacterCount = 147;
@@ -85,10 +90,11 @@ $(function(){
         if (characterCount >= maxElementCharacterCount) {
           maximum.css('color', '#8f0001');
           current.css('color', '#8f0001');
-          theCount.css('font-weight','bold');
+          theCount.css('font-weight', 'bold');
         } else {
-          maximum.css('color','#666');
-          theCount.css('font-weight','normal');
+          maximum.css('color', '#666');
+          current.css('color', 'rgb(30%, 68%, 31%)')
+          theCount.css('font-weight', 'normal');
         }
       });
     })
